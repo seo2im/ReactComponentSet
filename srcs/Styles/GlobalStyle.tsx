38185a036255f2b3972868a9a0747b082175b529
seo2im <seo2im6492@gmail.com>
2021-04-26 11:31:36 +0900

    Global Style(styled-component) Setting
    DO
    1. globe styling
    2. directory structure arrangement
    
    TODO
    1. Each Components is better

---
+++ srcs/Styles/GlobalStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle =  createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        
+        color: white;
+        background-color: black;
+    }
+`
+
+export default GlobalStyle
