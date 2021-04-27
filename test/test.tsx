8831d3b77d1a52ca12499932b58a002e9cac102e
seo2im <seo2im6492@gmail.com>
2021-04-27 09:33:11 +0900

    Testing library apply
    DO
    1. jest & react-testing-library connect
    2. test it work
    
    TODO
    1. Learn jest.config
    2. Learn what test in testing-library

---
+++ test/test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+import { ContextMenu } from '../srcs/Components'
+
+describe("<ContextMenu />", () => {
+    test("should?", async () => {
+        
+    })
+})
