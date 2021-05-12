bbc9f998179e472ffbb2b8d06bb321d7463f075f
seo2im <seo2im6492@gmail.com>
2021-05-13 00:10:36 +0900

    <refactor>:refactor autoComplete code - customHook으로 state 전부 변경 - 간단한 styling진행

--- srcs/Components/AutoComplete/AutoComplete.tsx
+++ srcs/Components/AutoComplete/AutoComplete.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Div, Input, AutoItems, Item } from './Style'
 import fetchData from './fetchData'
 
@@ -15,43 +15,73 @@ const debounce = (callback: (...args: any[]) => void, delayTime: number) => {
     }
 }
 
-const AutoComplete: React.FC = () => {
+const useAutoComplete = ()
+: [string, string[], string, 
+(text: string) => void,
+(display: string) => void,
+(overlay: boolean) => void,
+() => void,
+() => void, 
+(auto: string) => void] => {
     const [text, setText] = useState<string>('')
     const [autoText, setAutoText] = useState<string[]>([])
     const [display, setDisplay] = useState<string>('none')
     const [overlay, setOverlay] = useState<boolean>(false)
-    
+
     const getAutoText = debounce(async (text: string) => {
         const autos = await fetchData(text)
         setAutoText(autos)
     }, 500)
 
+    const showAuto = async () => {
+        getAutoText(text)
+        setDisplay('block')
+    }
 
+    const setAutoToText = (auto: string) => {
+        setText(auto)
+        setDisplay('none')
+    }
+    
+    const hideAuto = () => {
+        if (overlay) return
+        setDisplay('none')
+    }
+
+    return [text, autoText, display, setText, setDisplay, setOverlay, showAuto, hideAuto, setAutoToText]
+}
+
+const AutoComplete: React.FC = () => {
+    const [text, autoText, display,
+            setText, setDisplay, setOverlay,
+            showAuto, hideAuto, setAutoToText] = useAutoComplete()
+
+    const ref = useRef<HTMLInputElement>()
+    const [pos, setPos] = useState<number>(0)
+
+    useEffect(() => {
+        setPos(ref.current.getBoundingClientRect().height)
+    }, [])
+    
     return (
         <Div>
-            <Input 
+            <Input
+                ref={ref}
                 value={text}
                 onChange={e => setText(e.target.value)}
-                onKeyUp={() =>  {
-                    getAutoText(text)
-                    setDisplay('block')
-                }}
-                onFocus={() => setDisplay('block')}
-                onBlur={() => {
-                    if (overlay) return
-                    setDisplay('none')
-                }}
+                onKeyUp={() =>  showAuto()}
+                onFocus={() => showAuto()}
+                onBlur={() => hideAuto()}
                 />
-            <AutoItems display={display}
-                onMouseOver={() => setOverlay(true)}
+            <AutoItems display={display} pos={pos}
+                onMouseOver={() => {
+                    setOverlay(true)
+                }}
                 onMouseOut={() => setOverlay(false)}>
-                {autoText.map((text, i) => (
+                {autoText.map((auto, i) => (
                     <Item key={`item_${i}`}
-                        onClick={() => {
-                            setText(text)
-                            setDisplay('none')
-                            }}>
-                        {text}
+                        onClick={() => setAutoToText(auto)}>
+                        {auto}
                     </Item>
                 ))}
             </AutoItems>
--- srcs/Components/AutoComplete/Style.tsx
+++ srcs/Components/AutoComplete/Style.tsx
@@ -7,8 +7,15 @@ export const Input = styled.input`
 
 `
 export const Item = styled.div`
+    margin: 0.2rem 0.5rem;
 
+    color : white;
 `
-export const AutoItems = styled.div<{ display: string }>`
+export const AutoItems = styled.div<{ display: string, pos: number }>`
     display: ${({ display }) => display};
+
+    position: absolute;
+    top: ${({ pos }) => pos}px;
+
+    background-color: grey;
 `
