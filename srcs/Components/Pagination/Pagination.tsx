3ec02faff5c00d3735a67106699e882415e93d7d
seo2im <seo2im6492@gmail.com>
2021-05-17 20:40:12 +0900

    feat:basic setting

---
+++ srcs/Components/Pagination/Pagination.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Div } from './Style'
+
+const Pagination: React.FC = () => {
+    return (
+        <Div>
+
+        </Div>
+    )
+};
+
+export default Pagination;
